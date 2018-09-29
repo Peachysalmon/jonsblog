57d6511f4a4877b63724fb241655d8baececb976
Peachysalmon <info@jonathan-meier.com>
2018-09-29 20:15:03 +0200

    Added Contact & About Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'landing_pages/about'
+  get 'landing_pages/contact'
+  root 'landing_pages#index'
+  get 'landing_pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
