832930e4f909e3de2a81e30a19ad5cb88b704c82
Peachysalmon <info@jonathan-meier.com>
2018-09-30 21:10:26 +0200

    added Bootstrap and some styling

--- app/controllers/landing_pages_controller.rb
+++ app/controllers/landing_pages_controller.rb
@@ -1,4 +1,5 @@
 class LandingPagesController < ApplicationController
   def index
+    @articles = Article.all
   end
 end
---
+++ db/migrate/20180930182933_add_subtitles.rb
@@ -0,0 +1,5 @@
+class AddSubtitles < ActiveRecord::Migration[5.2]
+  def change
+    add_column :articles, :subtitle, :string
+  end
+end
